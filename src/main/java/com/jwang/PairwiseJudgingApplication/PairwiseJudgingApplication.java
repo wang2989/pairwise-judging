2b07d7acf5c0240174d53322f1a5b0476934b9b5
Josh Wang <joshrw42@gmail.com>
2023-12-12 16:22:08 -0500

    Initialized Spring Boot Web project

---
+++ src/main/java/com/jwang/PairwiseJudgingApplication/PairwiseJudgingApplication.java
@@ -0,0 +1,13 @@
+package com.jwang.PairwiseJudgingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairwiseJudgingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairwiseJudgingApplication.class, args);
+	}
+
+}
